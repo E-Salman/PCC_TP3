f6d34ac0628039c2e404f2a17da7c777667d13cf
Esteban Salman <Admin@Admin-PC>
2020-09-15 21:33:29 -0300

    Implementado un switch con una variable para seleccionar que ejercicio ejecutar

--- src/pc_tp3/Main.java
+++ src/pc_tp3/Main.java
@@ -28,15 +28,35 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        //1)
-        /*VerificarCuenta vc = new VerificarCuenta();
+        
+        int ejercicio = 1;        //Modificar para elegir que ejercicio ejecutar
+                                  //Ej: ejercicio = 1 ejecuta el primer ejercicio
+        switch(ejercicio){
+            case 1:
+                ejecutarEj1();
+                break;
+            case 2:
+                ejecutarEj2();
+                break;
+            case 3:
+                ejecutarEj3();
+                break;
+            case 4:
+                ejecutarEj4();
+                break;
+        }
+    }
+    
+    public static void ejecutarEj1(){
+        VerificarCuenta vc = new VerificarCuenta();
         Thread[] hilos = new Thread[2];
         hilos[0] = new Thread(vc, "Luis");
         hilos[1] = new Thread(vc, "Manuel");
-        for(int i = 0; i < 2; i++) hilos[i].start();*/
-        
-        //2)
-        /*Vida vida = new Vida();
+        for(int i = 0; i < 2; i++) hilos[i].start();
+    }
+    
+    public static void ejecutarEj2(){
+        Vida vida = new Vida();
         Thread[] hilos = new Thread[2];
         hilos[0] = new Thread(new Orco(vida), "orco");
         hilos[1] = new Thread(new Curandero(vida), "curandero");
@@ -50,24 +70,26 @@ public class Main {
         } catch (InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("Vida resultante: " + vida.obtenerVida());*/
-        
-        //3)
-        /*Turno turno = new Turno();
+        System.out.println("Vida resultante: " + vida.obtenerVida());
+    }
+    
+    public static void ejecutarEj3(){
+        Turno turno = new Turno();
         Thread[] hilos = new Thread[3];
         hilos[0] = new Thread(new Letra ('A', 1, turno));
         hilos[1] = new Thread(new Letra ('B', 2, turno));
         hilos[2] = new Thread(new Letra ('C', 3, turno));
-        for(int i = 0; i < 3; i++) hilos[i].start();*/
-        
-        //4)
-        /*Service service = new Service();
+        for(int i = 0; i < 3; i++) hilos[i].start();
+    }
+    
+    public static void ejecutarEj4(){
+        Service service = new Service();
         Thread[] hilos = new Thread[5];
         hilos[0] = new Thread(new Auto (service, 50), "Auto 1");
         hilos[1] = new Thread(new Auto (service, 125), "Auto 2");
         hilos[2] = new Thread(new Auto (service, 150), "Auto 3");
         hilos[3] = new Thread(new Auto (service, 250), "Auto 4");
         hilos[4] = new Thread(new Auto (service, 175), "Auto 5");
-        for(int i = 0; i < 5; i++) hilos[i].start();*/
-    }    
+        for(int i = 0; i < 5; i++) hilos[i].start();
+    }
 }
